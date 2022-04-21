c43df925d6cd6f5b3143610f695ad4a117bf1f6c
RiverRay <laolei@forkway.cn>
2022-04-21 14:17:16 +0800

    feat(JSR303): 增加DTO字段类型校验

--- local_map_restful/src/main/java/com/example/local_map_restful/User.java
+++ local_map_restful/src/main/java/com/example/local_map_restful/User.java
@@ -2,6 +2,9 @@ package com.example.local_map_restful;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.*;
 
 /**
  * @author river
@@ -9,8 +12,16 @@ import lombok.Data;
 @Data
 @Schema(description = "用户信息")
 public class User {
-    private Long id;
-    @Schema(description = "名称", required = true, example = "张三")
-    private String name;
-    private Integer age;
+	@Schema(description = "ID", required = true, example = "1")
+	private Long id;
+
+	@Schema(description = "名称", required = true, example = "张三")
+	@NotNull
+	private String name;
+
+	@Max(10)
+	@Min(1)
+	@Schema(description = "年龄", required = true, example = "10")
+	private Integer age;
+
 }
--- local_map_restful/src/main/java/com/example/local_map_restful/control/UserController.java
+++ local_map_restful/src/main/java/com/example/local_map_restful/control/UserController.java
@@ -2,10 +2,13 @@ package com.example.local_map_restful.control;
 
 import com.example.local_map_restful.User;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.*;
 
 /**
@@ -35,6 +38,7 @@ public class UserController {
      * @return List<User>
      */
     @GetMapping("/list")
+    @Operation(summary = "获取用户列表", description = "获取用户列表")
     public List<User> getUser() {
         return new ArrayList<>(users.values());
     }
@@ -45,7 +49,8 @@ public class UserController {
      * @return string
      */
     @PostMapping("/add")
-    public String addUser(@RequestBody User user) {
+    @Operation(summary = "新增用户", description = "新增用户")
+    public String addUser(@Valid @RequestBody User user) {
         users.put(user.getId(), user);
         return "success";
     }
@@ -56,6 +61,10 @@ public class UserController {
      * @return User
      */
     @GetMapping("/{id}")
+    @Parameters({
+            @Parameter(name = "id", description = "用户id", required = true)
+    })
+    @Operation(summary = "根据id获取用户", description = "根据id获取用户")
     public User getUser(@PathVariable Long id) {
         return users.get(id);
     }
@@ -70,6 +79,7 @@ public class UserController {
      * @return string
      */
     @PutMapping("/{id}")
+    @Operation(summary = "根据id修改用户", description = "根据id修改用户")
     public String putUser(@PathVariable Long id, @RequestBody User user) {
         User u = users.get(id);
         u.setName(user.getName());
@@ -85,6 +95,7 @@ public class UserController {
      * @return string
      */
     @DeleteMapping("/{id}")
+    @Operation(summary = "根据id删除用户", description = "根据id删除用户")
     public String deleteUser(@PathVariable Long id) {
         users.remove(id);
         return "success";
